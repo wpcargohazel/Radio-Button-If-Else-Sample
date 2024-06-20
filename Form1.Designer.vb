3d2485272d0515fa74d1c15be3bd36867fd7a0b5
Yuri Tetchu <140356918+wpcargohazel@users.noreply.github.com>
2024-06-20 22:33:38 +0800

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,213 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class aceform
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.ace1 = New System.Windows.Forms.RadioButton()
+        Me.ace2 = New System.Windows.Forms.RadioButton()
+        Me.ace3 = New System.Windows.Forms.RadioButton()
+        Me.ace4 = New System.Windows.Forms.RadioButton()
+        Me.ace5 = New System.Windows.Forms.RadioButton()
+        Me.acecheck1 = New System.Windows.Forms.CheckBox()
+        Me.acecheck2 = New System.Windows.Forms.CheckBox()
+        Me.acecheck3 = New System.Windows.Forms.CheckBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'TextBox2
+        '
+        Me.TextBox2.BackColor = System.Drawing.Color.LightSalmon
+        Me.TextBox2.Location = New System.Drawing.Point(167, 25)
+        Me.TextBox2.Multiline = True
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(142, 142)
+        Me.TextBox2.TabIndex = 14
+        '
+        'TextBox1
+        '
+        Me.TextBox1.BackColor = System.Drawing.Color.LightSalmon
+        Me.TextBox1.Location = New System.Drawing.Point(17, 20)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(126, 205)
+        Me.TextBox1.TabIndex = 13
+        '
+        'ace1
+        '
+        Me.ace1.BackColor = System.Drawing.Color.Snow
+        Me.ace1.Font = New System.Drawing.Font("Impact", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ace1.Location = New System.Drawing.Point(22, 25)
+        Me.ace1.Name = "ace1"
+        Me.ace1.Size = New System.Drawing.Size(112, 23)
+        Me.ace1.TabIndex = 4
+        Me.ace1.TabStop = True
+        Me.ace1.Text = "RED"
+        Me.ace1.UseVisualStyleBackColor = False
+        '
+        'ace2
+        '
+        Me.ace2.BackColor = System.Drawing.Color.Snow
+        Me.ace2.Font = New System.Drawing.Font("Impact", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ace2.Location = New System.Drawing.Point(22, 65)
+        Me.ace2.Name = "ace2"
+        Me.ace2.Size = New System.Drawing.Size(112, 23)
+        Me.ace2.TabIndex = 5
+        Me.ace2.TabStop = True
+        Me.ace2.Text = "GREEN"
+        Me.ace2.UseVisualStyleBackColor = False
+        '
+        'ace3
+        '
+        Me.ace3.BackColor = System.Drawing.Color.Snow
+        Me.ace3.Font = New System.Drawing.Font("Impact", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ace3.Location = New System.Drawing.Point(22, 110)
+        Me.ace3.Name = "ace3"
+        Me.ace3.Size = New System.Drawing.Size(112, 23)
+        Me.ace3.TabIndex = 6
+        Me.ace3.TabStop = True
+        Me.ace3.Text = "BLUE"
+        Me.ace3.UseVisualStyleBackColor = False
+        '
+        'ace4
+        '
+        Me.ace4.BackColor = System.Drawing.Color.Snow
+        Me.ace4.Font = New System.Drawing.Font("Impact", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ace4.Location = New System.Drawing.Point(22, 153)
+        Me.ace4.Name = "ace4"
+        Me.ace4.Size = New System.Drawing.Size(112, 23)
+        Me.ace4.TabIndex = 7
+        Me.ace4.TabStop = True
+        Me.ace4.Text = "PINK"
+        Me.ace4.UseVisualStyleBackColor = False
+        '
+        'ace5
+        '
+        Me.ace5.BackColor = System.Drawing.Color.Snow
+        Me.ace5.Font = New System.Drawing.Font("Impact", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ace5.Location = New System.Drawing.Point(22, 193)
+        Me.ace5.Name = "ace5"
+        Me.ace5.Size = New System.Drawing.Size(112, 23)
+        Me.ace5.TabIndex = 8
+        Me.ace5.TabStop = True
+        Me.ace5.Text = "YELLOW"
+        Me.ace5.UseVisualStyleBackColor = False
+        '
+        'acecheck1
+        '
+        Me.acecheck1.AutoSize = True
+        Me.acecheck1.BackColor = System.Drawing.Color.LightCyan
+        Me.acecheck1.Font = New System.Drawing.Font("Stencil", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.acecheck1.Location = New System.Drawing.Point(177, 35)
+        Me.acecheck1.Name = "acecheck1"
+        Me.acecheck1.Size = New System.Drawing.Size(123, 22)
+        Me.acecheck1.TabIndex = 9
+        Me.acecheck1.Text = "CheckBox1"
+        Me.acecheck1.UseVisualStyleBackColor = False
+        '
+        'acecheck2
+        '
+        Me.acecheck2.AutoSize = True
+        Me.acecheck2.BackColor = System.Drawing.Color.LightCyan
+        Me.acecheck2.Font = New System.Drawing.Font("Stencil", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.acecheck2.Location = New System.Drawing.Point(177, 85)
+        Me.acecheck2.Name = "acecheck2"
+        Me.acecheck2.Size = New System.Drawing.Size(123, 22)
+        Me.acecheck2.TabIndex = 10
+        Me.acecheck2.Text = "CheckBox2"
+        Me.acecheck2.UseVisualStyleBackColor = False
+        '
+        'acecheck3
+        '
+        Me.acecheck3.AutoSize = True
+        Me.acecheck3.BackColor = System.Drawing.Color.LightCyan
+        Me.acecheck3.Font = New System.Drawing.Font("Stencil", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.acecheck3.Location = New System.Drawing.Point(177, 137)
+        Me.acecheck3.Name = "acecheck3"
+        Me.acecheck3.Size = New System.Drawing.Size(123, 22)
+        Me.acecheck3.TabIndex = 11
+        Me.acecheck3.Text = "CheckBox3"
+        Me.acecheck3.UseVisualStyleBackColor = False
+        '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.Color.Salmon
+        Me.Button2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.Location = New System.Drawing.Point(171, 175)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(133, 50)
+        Me.Button2.TabIndex = 12
+        Me.Button2.Text = "GO"
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.Coral
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(88, 232)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(128, 47)
+        Me.Button1.TabIndex = 15
+        Me.Button1.Text = "Close"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'aceform
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.LightSkyBlue
+        Me.ClientSize = New System.Drawing.Size(338, 337)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.acecheck3)
+        Me.Controls.Add(Me.acecheck2)
+        Me.Controls.Add(Me.acecheck1)
+        Me.Controls.Add(Me.ace5)
+        Me.Controls.Add(Me.ace4)
+        Me.Controls.Add(Me.ace3)
+        Me.Controls.Add(Me.ace2)
+        Me.Controls.Add(Me.ace1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.TextBox2)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.Name = "aceform"
+        Me.Text = "Radio Button"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents ace1 As System.Windows.Forms.RadioButton
+    Friend WithEvents ace2 As System.Windows.Forms.RadioButton
+    Friend WithEvents ace3 As System.Windows.Forms.RadioButton
+    Friend WithEvents ace4 As System.Windows.Forms.RadioButton
+    Friend WithEvents ace5 As System.Windows.Forms.RadioButton
+    Friend WithEvents acecheck1 As System.Windows.Forms.CheckBox
+    Friend WithEvents acecheck2 As System.Windows.Forms.CheckBox
+    Friend WithEvents acecheck3 As System.Windows.Forms.CheckBox
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,70 @@
+﻿Public Class aceform
+
+    Private Sub ace1_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ace1.CheckedChanged
+        If ace1.Checked = True Then
+            MsgBox("You have selected Red !!")
+            BackColor = Color.Red
+            ace1.BackColor = Color.Red
+            Exit Sub
+        End If
+    End Sub
+
+    Private Sub ace3_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ace3.CheckedChanged
+        If ace3.Checked = True Then
+            MsgBox("You have selected Blue !!")
+            BackColor = Color.Blue
+            ace3.BackColor = Color.Blue
+            Exit Sub
+        End If
+    End Sub
+
+    Private Sub ace5_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ace5.CheckedChanged
+        If ace5.Checked = True Then
+            MsgBox("You have selected Yellow !!")
+            BackColor = Color.Yellow
+            ace5.BackColor = Color.Yellow
+            Exit Sub
+        End If
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        Dim msg As String = ""
+        If acecheck1.Checked = True Then
+            msg = "net-informations.com"
+        End If
+        If acecheck2.Checked = True Then
+            msg = "vb.net-informations.com"
+        End If
+        If acecheck3.Checked = True Then
+            msg = "csharp.net-informations.com"
+        End If
+        If msg.Length > 0 Then
+            MsgBox(msg & "selected")
+        Else
+            MsgBox("No CheckBox Selected.")
+        End If
+        acecheck1.ThreeState = True
+    End Sub
+
+    Private Sub ace2_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ace2.CheckedChanged
+        If ace2.Checked = True Then
+            MsgBox("You have selected Green !!")
+            BackColor = Color.Green
+            ace2.BackColor = Color.Green
+            Exit Sub
+        End If
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Close()
+    End Sub
+
+    Private Sub ace4_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ace4.CheckedChanged
+        If ace4.Checked = True Then
+            MsgBox("You have selected Pink !!")
+            BackColor = Color.Pink
+            ace4.BackColor = Color.Pink
+            Exit Sub
+        End If
+    End Sub
+End Class
